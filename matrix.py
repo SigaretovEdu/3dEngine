fe56fdfe0476172e926e16ef4a82f72c14ae536a
Goryasha <90152178+Goryasha@users.noreply.github.com>
2021-12-17 12:40:24 +0300

    Update matrix.py
    Напиши где ссылки на все взаимодействия с матрицами

--- matrix.py
+++ matrix.py
@@ -1,43 +1,55 @@
-import numpy as np
 import math
+import numpy as np
 
-
-def translate(pos):
-    tx, ty, tz = pos
-    return np.array([
-        [1, 0, 0, 0],
-        [0, 1, 0, 0],
-        [0, 0, 1, 0],
-        [tx, ty, tz, 1]
+def move_to(coord):
+    x2, y2, z2 = coord
+    pos2=np.array([
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [x2,y2,z2,1]
     ])
+    return pos2
 
 
-def rotate_x(a):
-    return np.array([
-        [1, 0, 0, 0],
-        [0, math.cos(a), math.sin(a), 0],
-        [0, -math.sin(a), math.cos(a), 0],
-        [0, 0, 0, 1]
+def rotate_around_x(alfa):
+    pos2=np.array([
+        [1,0,0,0],
+        [0,math.cos(alfa),math.sin(alfa),0],
+        [0,-math.cos(alfa),math.sin(alfa),0],
+        [0,0,0,1]
     ])
+    return pos2
 
 
-def rotate_y(a):
-    return np.array([
-        [math.cos(a), 0, -math.sin(a), 0],
-        [0, 1, 0, 0],
-        [math.sin(a), 0, math.cos(a), 0],
-        [0, 0, 0, 1]
+def rotate_around_y(alfa):
+    pos2=np.array([
+        [math.cos(alfa),0,-math.sin(alfa),0],
+        [0,1,0,0],
+        [math.sin(alfa),0,math.cos(alfa),0],
+        [0,0,0,1]
     ])
+    return pos2
 
 
-def rotate_z(a):
-    return np.array([
-        [math.cos(a), math.sin(a), 0, 0],
-        [-math.sin(a), math.cos(a), 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
+def rotate_around_z(alfa):
+    pos2=np.array([
+        [math.cos(alfa),math.sin(alfa),0,0],
+        [-math.sin(alfa),math.cos(alfa),0,0],
+        [0,0,1,0],
+        [0,0,0,1]
     ])
+    return pos2
 
+
+def scale_change(a):
+    pos2=np.array([
+        [a,0,0,0],
+        [0,a,0,0],
+        [0,0,a,0],
+        [0,0,0,1]
+    ])
+    return pos2
 
 def scale(n):
     return np.array([
